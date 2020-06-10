be2b0f44a04dc0e93973d28a945d85842e5e7dfe
Yauheni Sheuchuk <shevchuked@tut.by>
2020-06-10 16:42:21 +0300

    feat: prepare to beautiful DnD

---
+++ src/html/HTML.jsx
@@ -0,0 +1,63 @@
+import React, { useState } from 'react';
+import './HTML.css';
+
+const todos = [
+	{
+		taskID: 1,
+		task: 'Walk the walk'
+	},
+	{
+		taskID: 2,
+		task: 'Talk the talk'
+	},
+	{
+		taskID: 3,
+		task: 'Jump the jump'
+	}
+];
+
+const HTML = () => {
+	const [state, setState] = useState(todos);
+	const [completedTasks, setCompletedTasks] = useState([]);
+	const [draggedTask, setDraggedTask] = useState({ });
+
+	const onDrag = (event, todo) => {
+		event.preventDefault();
+		setDraggedTask(todo);
+	}
+	const onDragOver = (event) => {
+		event.preventDefault();
+	}
+	const onDrop = (event ) => {
+		setCompletedTasks([...completedTasks, draggedTask]);
+		setState(state.filter(task => task.taskID !== draggedTask.taskID));
+		setDraggedTask({});
+	}
+
+	return (
+		<div className="HTML">
+			<h1>React HTML5 Drag and Drop</h1>
+			<div className="todos">
+				{ state.map(todo =>
+						<div
+							key={todo.taskID}
+							draggable={true}
+							onDrag={(e) => onDrag(e, todo)}>
+							{todo.task}
+						</div> )}
+			</div>
+			<div
+				className="done"
+				onDrop={(e) => onDrop(e)}
+				onDragOver={(e) => onDragOver(e)}>
+				{completedTasks.map((task, index) =>
+					<div key={task.taskID} >
+						{task.task}
+					</div>
+				)}
+			</div>
+		</div>
+	);
+};
+
+export default HTML;
