94af10a576f7a81ad429de293405221d5eb899e4
Yauheni Sheuchuk <shevchuked@tut.by>
2020-06-11 12:23:41 +0300

    feat: add DragDropContext, Draggable, Droppable

--- src/bdnd/BDnD.jsx
+++ src/bdnd/BDnD.jsx
@@ -1,18 +1,25 @@
 import React from 'react';
 import initialData from './initial-data';
 import Column from './innerComponents/Column';
+import { DragDropContext } from 'react-beautiful-dnd';
 
 class BDnD extends React.Component {
 	state = initialData;
 
+	onDragEnd = result => {
+
+	}
+
 	render() {
 		return (
-			this.state.columnOrder.map(columnId => {
-				const column = this.state.columns[columnId];
-				const tasks = column.taskIds.map(taskId => this.state.tasks[taskId]);
+			<DragDropContext onDragEnd={this.onDragEnd}>
+				{this.state.columnOrder.map(columnId => {
+					const column = this.state.columns[columnId];
+					const tasks = column.taskIds.map(taskId => this.state.tasks[taskId]);
 
-				return <Column key={column.id} column={column} tasks={tasks} />;
-			})
+					return <Column key={column.id} column={column} tasks={tasks} />;
+				})}
+			</DragDropContext>
 		);
 	}
 }
