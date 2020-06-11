357d0588199acaf4db2035cc98c61dea3dc21e76
Yauheni Sheuchuk <shevchuked@tut.by>
2020-06-11 11:45:03 +0300

    feat: add react-beautiful-dnd

--- src/bdnd/innerComponents/Task.jsx
+++ src/bdnd/innerComponents/Task.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const Container = styled.div``;
+const Container = styled.div`
+	border: 1px solid lightgray;
+	border-radius: 2px;
+	padding: 8px;
+	margin-bottom: 8px;
+`;
 
 export default class Task extends React.Component {
 	render() {
