84ca8e6651a88a72b3444f78ab4eba13ea5a21d9
Yauheni Sheuchuk <shevchuked@tut.by>
2020-06-16 14:54:44 +0300

    feat: add ability to change columns order by dragging a column

--- src/bdnd/BDnD.jsx
+++ src/bdnd/BDnD.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
 import initialData from './initial-data';
 import Column from './innerComponents/Column';
-import { DragDropContext } from 'react-beautiful-dnd';
+import { DragDropContext, Droppable } from 'react-beautiful-dnd';
+import styled from 'styled-components';
+
+const Columns = styled.div`
+	background-color: skyblue;
+	padding: 20px;
+`;
 
 class BDnD extends React.Component {
 	state = initialData;
@@ -9,51 +15,74 @@ class BDnD extends React.Component {
 	onDragEnd = result => {
 		const {destination, source, draggableId } = result;
 		if (!destination) return;
-		if (
-			destination.droppableId === source.droppableId &&
-			destination.index === source.index
-		) return;
-		console.log('destination ',destination, 'source ', source);
-		if (destination.droppableId !== source.droppableId) {
-			const prefColumn = this.state.columns[source.droppableId];
-			const newTaskIds = Array.from(prefColumn.taskIds);
+		if (source.droppableId === 'body') {
+			if (destination.index === source.index) return;
+			const { columnOrder } = this.state;
+			columnOrder.splice(source.index, 1);
+			columnOrder.splice(destination.index, 0, draggableId);
+			this.setState({columnOrder});
+		} else {
+			if (
+				destination.droppableId === source.droppableId &&
+				destination.index === source.index
+			) return;
+			console.log('destination ',destination, 'source ', source);
+			if (destination.droppableId !== source.droppableId) {
+				const prefColumn = this.state.columns[source.droppableId];
+				const newTaskIds = Array.from(prefColumn.taskIds);
+				newTaskIds.splice(source.index, 1);
+				prefColumn.taskIds = newTaskIds;
+				const newColumn = this.state.columns[destination.droppableId];
+				const taskIds = Array.from(newColumn.taskIds);
+				taskIds.splice(destination.index, 0, draggableId);
+				newColumn.taskIds = taskIds;
+				this.setState({clumns:
+						{
+							...this.state.columns,
+							[source.droppableId]: {...prefColumn},
+							[destination.droppableId]: {...newColumn},
+						},
+				} );
+				return;
+			}
+			const column = this.state.columns[destination.droppableId];
+			const newTaskIds = Array.from(column.taskIds);
 			newTaskIds.splice(source.index, 1);
-			prefColumn.taskIds = newTaskIds;
-			const newColumn = this.state.columns[destination.droppableId];
-			const taskIds = Array.from(newColumn.taskIds);
-			taskIds.splice(destination.index, 0, draggableId);
-			newColumn.taskIds = taskIds;
+			newTaskIds.splice(destination.index, 0, draggableId);
+			column.taskIds = newTaskIds;
 			this.setState({clumns:
 					{
 						...this.state.columns,
-						[source.droppableId]: {...prefColumn},
-						[destination.droppableId]: {...newColumn},
+						[destination.droppableId]: {...column},
 					},
 			} );
-			return;
 		}
-		const column = this.state.columns[destination.droppableId];
-		const newTaskIds = Array.from(column.taskIds);
-		newTaskIds.splice(source.index, 1);
-		newTaskIds.splice(destination.index, 0, draggableId);
-		column.taskIds = newTaskIds;
-		this.setState({clumns:
-				{
-					...this.state.columns,
-					[destination.droppableId]: {...column},
-				},
-		} );
 	}
 
 	render() {
 		return (
 			<DragDropContext onDragEnd={this.onDragEnd}>
-				{this.state.columnOrder.map(columnId => {
-					const column = this.state.columns[columnId];
-					const tasks = column.taskIds.map(taskId => this.state.tasks[taskId]);
+				<Droppable droppableId={'body'}>
+					{(provided) => (
+						<Columns ref={provided.innerRef}
+								 {...provided.droppableProps}>
+							{
+								this.state.columnOrder.map((columnId, index) => {
+									const column = this.state.columns[columnId];
+									const tasks = column.taskIds.map(taskId => this.state.tasks[taskId]);
 
-					return <Column key={column.id} column={column} tasks={tasks} />;
-				})}
+									return <Column
+										key={column.id}
+										column={column}
+										tasks={tasks}
+										index={index}
+									/>;
+								})
+							}
+							{provided.placeholder}
+						</Columns>
+					)}
+				</Droppable>
 			</DragDropContext>
 		);
 	}
--- src/bdnd/innerComponents/Column.jsx
+++ src/bdnd/innerComponents/Column.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import styled from 'styled-components';
-import { Droppable } from 'react-beautiful-dnd';
+import { Droppable, Draggable } from 'react-beautiful-dnd';
 import Task from './Task';
 
 const Container = styled.div`
 	margin: 8px;
-	border: 1px solid lightgray;
+	border: 3px solid lightgray;
 	border-radius: 2px;
+	background-color: #a3c9ad;
 `;
 const Title = styled.h3`
 	padding: 8px;
@@ -19,23 +20,30 @@ const TaskList = styled.div`
 export default class Column extends React.Component {
 	render() {
 		return(
-			<Container>
-				<Title>{this.props.column.title}</Title>
-				<Droppable droppableId={this.props.column.id}>
-					{(provided, snapshot) => (
-						<TaskList
-							ref={provided.innerRef}
-							{...provided.droppableProps}
-							isDraggingOver={snapshot.isDraggingOver}
-						>
-							{this.props.tasks.map((task, index) => {
-								return <Task key={task.id} task={task} index={index} />
-							})}
-							{provided.placeholder}
-						</TaskList>
-					)}
-				</Droppable>
-			</Container>
+			<Draggable draggableId={this.props.column.id} index={this.props.index}>
+				{(provided) => (
+					<Container ref={provided.innerRef}
+							   {...provided.draggableProps}
+							   {...provided.dragHandleProps}
+					>
+						<Title>{this.props.column.title}</Title>
+						<Droppable droppableId={this.props.column.id}>
+							{(provided, snapshot) => (
+								<TaskList
+									ref={provided.innerRef}
+									{...provided.droppableProps}
+									isDraggingOver={snapshot.isDraggingOver}
+								>
+									{this.props.tasks.map((task, index) => {
+										return <Task key={task.id} task={task} index={index} />
+									})}
+									{provided.placeholder}
+								</TaskList>
+							)}
+						</Droppable>
+					</Container>
+				)}
+			</Draggable>
 		);
 	}
 }
